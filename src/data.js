9b199fbc4dd6fc8c51fb1b67e1dbadf35b70396d
dewicz <dominika.dewicz@hotmail.com>
2022-07-05 22:08:15 +0100

    intro, about and product components

---
+++ src/data.js
@@ -0,0 +1,35 @@
+import me from "./images/Spock_at_console.jpg"
+
+
+export const products = [
+    {
+      id: 1,
+      img: me,
+      link: "http://lama.dev",
+    },
+    {
+      id: 2,
+      img: "",
+      link: "http://lama.dev",
+    },
+    {
+      id: 3,
+      img: "",
+      link: "http://lama.dev",
+    },
+    {
+      id: 4,
+      img: "",
+      link: "http://lama.dev",
+    },
+    {
+      id: 5,
+      img: "",
+      link: "http://lama.dev",
+    },
+    {
+      id: 6,
+      img: "",
+      link: "http://lama.dev",
+    },
+  ];
